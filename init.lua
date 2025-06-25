59814e346ee19d9d3e7672462fa9079d28c6b0c8
Hans <hansowen.m@gmail.com>
2025-06-25 21:42:32 +0100

    added lua lsp

--- init.lua
+++ init.lua
@@ -54,18 +54,33 @@ require('lazy').setup({
 	"nvim-telescope/telescope.nvim", tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	
+
 	-- Treesitter for syntax highlighting
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 
 	-- LSP setup
-	{"neovim/nvim-lspconfig"}
-})
+	{"neovim/nvim-lspconfig"},
 
+	-- Mason LSP installer
+	-- {	
+	-- 	"mason-org/mason.nvim",
+	--	opts = {}
+	--}
+})
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files.' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep.' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers.' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags.' })
+
+require'lspconfig'.lua_ls.setup{
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+}
 
